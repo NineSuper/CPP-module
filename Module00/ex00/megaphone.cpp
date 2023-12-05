5d6ea8baacc7c4e551d6543d01ab244ff4cf0c0f
De Los Muros Tom <nine@MacBook-Pro-2.local>
2023-12-05 17:08:57 +0100

    ex00 and Makefile V2.2

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -6,7 +6,36 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 12:40:23 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/12/04 12:48:55 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/12/05 17:07:41 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+void	ft_toupeur(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		std::cout << (char)(c - 32);
+	else
+		std::cout << c;
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (++i < argc)
+	{
+		int j = -1;
+		while (argv[i][++j])
+			ft_toupeur(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
