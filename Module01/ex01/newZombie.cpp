b7aeaa8ba2c233cec8d515e9883e817219153484
De Los Muros Tom <nine@MacBook-Pro-2.local>
2023-12-12 11:37:11 +0100

    Cpp Module 01/ex01

--- Module01/ex01/newZombie.cpp
+++ Module01/ex01/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 12:53:47 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/12/07 16:39:58 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/12/12 11:32:37 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ Zombie*	Zombie::zombieHorde(int n, std::string name)
 	
 	while (++i < n)
 		horde[i] = Zombie(name);
-	horde[0].randomChump("Zombie number one");
-	horde[1].randomChump("Zombie number two");
 	return (&horde[0]);
 }
 
