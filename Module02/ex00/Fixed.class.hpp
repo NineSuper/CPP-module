b0725a74e9a989846f93a0a14dc2a9a4aabad1e1
NineSuper <NineSuper66@gmail.com>
2023-12-14 17:38:24 +0100

    Module02/ex00

---
+++ Module02/ex00/Fixed.class.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.class.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 17:35:37 by tde-los-          #+#    #+#             */
+/*   Updated: 2023/12/14 17:37:41 by tde-los-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		/* data */
+	public:
+		Fixed(void);
+		~Fixed(void);
+};
+
+#endif
